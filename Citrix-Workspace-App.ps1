630ca14ce31f2396470e69e5fe1c37f1b0f2e321
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2021-04-15 09:38:57 +0200

    Create Citrix-Workspace-App.ps1

---
+++ Citrix-Workspace-App.ps1
@@ -0,0 +1,81 @@
+﻿#Requires -RunAsAdministrator
+<#
+*************************************************************************************************
+Name:               Citrix-Workspace-App
+Author:             Kasper Johansen
+Website:            https://virtualwarlock.net            
+
+*************************************************************************************************
+.DESCRIPTION
+    This script installs the latest Citrix Workspace App 
+    using the Evergreen module created by Aaron Parker, Bronson Mangan and Trond Eric Haavarstein
+    https://github.com/aaronparker/Evergreen
+
+*************************************************************************************************
+#>
+
+# Clear screen
+cls
+
+# Configure security protocol to use TLS 1.2 for new connections
+Write-Host "Configuring TLS1.2 security protocol for new connections" -ForegroundColor Cyan
+Write-Host ""
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
+
+# Download latest NuGet Package Provider
+If (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget"))
+{
+Write-Host "Installing latest NuGet Package Provider" -ForegroundColor Cyan
+Write-Host ""
+Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies | Out-Null
+}    
+
+# Download latest Evergreen module
+Write-Host "Installing latest Evergreen module" -ForegroundColor Cyan
+Write-Host ""
+If (!(Get-Module -ListAvailable -Name Evergreen))
+{
+Install-Module Evergreen -Force | Import-Module Evergreen
+}
+else
+{
+Update-Module Evergreen -Force
+}
+
+# Configure Evergreen variables
+$Vendor = "Citrix"
+$Product = "Workspace App"
+$EvergreenApp = Get-EvergreenApp -Name CitrixWorkspaceApp | where {$_.Title -eq "Citrix Workspace - Current Release"}
+$EvergreenAppInstaller = Split-Path -Path $EvergreenApp.Uri -Leaf
+$EvergreenAppURL = $EvergreenApp.uri
+$EvergreenAppVersion = $EvergreenApp.Version
+$Destination = "C:\Temp\$Vendor $Product"
+
+# Application install arguments 
+# This will prevent desktop and taskbar shortcuts from appearing during first logon 
+$InstallArguments = "/noreboot /silent ENABLE_SSON=True EnableCEIP=False "
+
+# Create destination folder, if not exist
+If (!(Test-Path -Path $Destination))
+{
+Write-Host "Creating $Destination" -ForegroundColor Cyan
+Write-Host ""
+New-Item -ItemType Directory -Path $Destination | Out-Null
+}
+
+# Download and deploy application
+Write-Host "Downloading latest $Vendor $Product release" -ForegroundColor Cyan
+Write-Host ""
+Invoke-WebRequest -UseBasicParsing -Uri $EvergreenAppURL -OutFile $Destination\$EvergreenAppInstaller
+
+Write-Host "Installing $Vendor $Product v$EvergreenAppVersion" -ForegroundColor Cyan
+Write-Host ""
+Start-Process -FilePath $Destination\$EvergreenAppInstaller -Wait -ArgumentList $InstallArguments
+
+# Application post deployment tasks
+Write-Host "Applying Microsoft Edge post setup customizations" -ForegroundColor Cyan
+
+# Disable Add Store popup
+New-Item -Path "HKLM:SOFTWARE\Wow6432Node\Policies" -Name Citrix -Force | Out-Null
+New-ItemProperty -Path "HKLM:SOFTWARE\Wow6432Node\Policies\Citrix" -Name EnableX1FTU -Value 0 | Out-Null
+#New-ItemProperty -Path "HKLM:SOFTWARE\Wow6432Node\Citrix\Dazzle" -Name AllowAddStore -Value N -Force | Out-Null
