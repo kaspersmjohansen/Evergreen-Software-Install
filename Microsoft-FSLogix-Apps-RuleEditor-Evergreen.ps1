8795d777388f615f0cef4a070d2e2c5947d82a64
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2021-05-01 10:19:11 +0200

    Create Microsoft-FSLogix-Apps-RuleEditor-Evergreen.ps1

---
+++ Microsoft-FSLogix-Apps-RuleEditor-Evergreen.ps1
@@ -0,0 +1,79 @@
+﻿#Requires -RunAsAdministrator
+<#
+*************************************************************************************************
+Name:               Microsoft-FSLogix-Apps-RuleEditor-Evergreen
+Author:             Kasper Johansen
+Website:            https://virtualwarlock.net            
+
+*************************************************************************************************
+
+.DESCRIPTION
+    This script installs the latest Microsoft FSLogix Apps Agent 
+    using the Evergreen module created by Aaron Parker, Bronson Mangan and Trond Eric Haavarstein
+    https://github.com/aaronparker/Evergreen
+
+*************************************************************************************************
+#>
+
+# Clear screen
+cls
+
+# Configure security protocol to use TLS 1.2 for new connections
+Write-Host "Configuring TLS1.2 security protocol for new connections" -ForegroundColor Cyan
+Write-Host ""
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
+
+# Download latest NuGet Package Provider
+If (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget"))
+{
+Write-Host "Installing latest NuGet Package Provider" -ForegroundColor Cyan
+Write-Host ""
+Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies | Out-Null
+}    
+
+# Download latest Evergreen module
+Write-Host "Installing latest Evergreen module" -ForegroundColor Cyan
+Write-Host ""
+If (!(Get-Module -ListAvailable -Name Evergreen))
+{
+Install-Module Evergreen -Force | Import-Module Evergreen
+}
+else
+{
+Update-Module Evergreen -Force
+}
+
+# Configure Evergreen variables
+$Vendor = "Microsoft"
+$Product = "FSLogix Apps RuleEditor"
+$EvergreenApp = Get-EvergreenApp -Name MicrosoftFSLogixApps | Sort-Object -Property Version -Descending | Select-Object -First 1
+$EvergreenAppInstaller = Split-Path -Path $EvergreenApp.Uri -Leaf
+$EvergreenAppURL = $EvergreenApp.uri
+$EvergreenAppVersion = $EvergreenApp.Version
+$Destination = "C:\Temp\$Vendor $Product"
+$OS = (Get-WmiObject Win32_OperatingSystem).Caption
+
+# Application install arguments 
+# This will prevent desktop and taskbar shortcuts from appearing during first logon 
+$InstallArguments = "/install /quiet /norestart"
+
+# Create destination folder, if not exist
+If (!(Test-Path -Path $Destination))
+{
+Write-Host "Creating $Destination" -ForegroundColor Cyan
+Write-Host ""
+New-Item -ItemType Directory -Path $Destination | Out-Null
+}
+
+# Download and deploy application
+Write-Host "Downloading latest $Vendor $Product release" -ForegroundColor Cyan
+Write-Host ""
+Invoke-WebRequest -UseBasicParsing -Uri $EvergreenAppURL -OutFile $Destination\$EvergreenAppInstaller
+
+# Expand the FSLogix Apps Zip file
+Expand-Archive -Path $Destination\$EvergreenAppInstaller -DestinationPath "$Destination"
+
+# Deploy Microsoft FSLogix Apps Agent
+Write-Host "Installing $Vendor $Product v$EvergreenAppVersion" -ForegroundColor Cyan
+Write-Host ""
+Start-Process -FilePath "$Destination\x64\Release\FSLogixAppsRuleEditorSetup.exe" -Wait -ArgumentList $InstallArguments
